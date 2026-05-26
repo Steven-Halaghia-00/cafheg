3cc10d4c7c6638c1da8d7ab2f80898401c9f64f2
vuillthe <contact@theovuillaume.ch>
2026-05-26 21:27:30 +0200

    test(exercise-4): add initial integration test placeholder

---
+++ src/integration-test/java/ch/hearc/cafheg/MyTestsIT.java
@@ -0,0 +1,13 @@
+package ch.hearc.cafheg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MyTestsIT {
+
+    @Test
+    void placeholderTest() {
+        assertEquals(1, 1);
+    }
+}
